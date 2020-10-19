c65e45ffc154696970b3d0767e9f3a71c70e6c90
KollerZx <henrique.koller@gmail.com>
2020-10-19 20:36:24 -0300

    criaçao do script para calcular o preço medio

---
+++ script.js
@@ -0,0 +1,13 @@
+function calcule() {
+  let precoMedio = parseFloat(document.getElementById("precoMedio").value);
+  let qtde = parseFloat(document.getElementById("qtde").value);
+  let cotacao = parseFloat(document.getElementById("cotacao").value);
+  let qtdeCompra = parseFloat(document.getElementById("qtdCompra").value);
+  let custo = precoMedio * qtde;
+  let compra = qtdeCompra * cotacao;
+  let custoNovo = compra + custo;
+  let qtdAt = qtde + qtdeCompra;
+  let pm = custoNovo / qtdAt;
+
+  return (document.getElementById("resultado").innerHTML = "R$ " + pm);
+}
